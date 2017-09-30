9125da9c818201a99a831e873ffdf90b0089ba51
qinysy <799560348@qq.com>
2017-09-30 11:31:44 +0800

    add ex1.py

---
+++ ex1.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+#coding: utf-8
+#__author:qinzhang
+
+
+username=input('请输出你的用户名: ')
+print(username,type(username))
+
+#在python2中有raw_input与python3的input是一个意思
+#特点是：把所有用户的输入都转成字符串类型
+#username=raw_input('please input your name: ')
+#print(username,type(username))
+
+#在python2中有input
+#特点是：把用户输入什么类型,就存成什么类型
+#x=input('>>: ')
+#print(x,type(x))
+
+#接受用户输入的用户名和密码
+#user=input('用户名: ')
+#password=input('密码: ')
+
+#print(user)
+#print(password)
+
+
+	
+
